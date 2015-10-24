588280701c600eca64bb4a1642854d409cebabf0
Samuel Eng <sam.eng@icloud.com>
2015-10-24 16:42:40 -0400

    updated about screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,7 @@ public class MainMenu : MonoBehaviour {
 
 	public GameManager manager;
 	public GameObject gameOverScreen;
+	public GameObject aboutScreen;
 
 
 	//call this function when startGame button is pressed
@@ -20,7 +21,15 @@ public class MainMenu : MonoBehaviour {
 
 	public void StartMainMenu(){
 		gameOverScreen.gameObject.SetActive(false);
+		aboutScreen.gameObject.SetActive(false);
 		this.gameObject.SetActive(true);
+		manager.scoreText.gameObject.SetActive(true);
+	}
+
+	public void AboutScreen(){
+		aboutScreen.gameObject.SetActive(true);
+		this.gameObject.SetActive(false);
+		manager.scoreText.gameObject.SetActive(false);
 	}
 
 	public void StartGameOverScreen(){
