b3c8be920e451d47ffd5196583633b8bc5af4ea7
Samuel Eng <sam.eng@icloud.com>
2015-10-24 11:34:23 -0400

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public MainMenu menu;
+
+	private bool isGameOver = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isGameOver){
+			InGameUpdate();
+		}
+	}
+
+	void InGameUpdate(){
+
+
+	}
+
+
+	public void BeginGame(){
+		Debug.Log("Game begin");
+		isGameOver = false;
+	}
+
+
+	public void EndGame(){
+		Debug.Log("End game!!");
+		isGameOver = true;
+		menu.StartMainMenu();
+	}
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameManager manager;
+
+
+	//call this function when startGame button is pressed
+	public void StartGame(){
+		Debug.Log("HEY YOU PRESSED THE BUTTON");
+		StartGameManager();
+		this.gameObject.SetActive(false);
+	}
+
+	void StartGameManager(){
+		manager.BeginGame();
+	}
+
+	public void StartMainMenu(){
+		this.gameObject.SetActive(true);
+	}
+}
