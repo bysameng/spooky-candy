60f3ceb82b83f7ae0f838ccd5b83281bb75dfbc1
Mike Williams <mikewilliams76901@gmail.com>
2015-10-24 12:31:34 -0400

    Added asset movement script

---
+++ Assets/Scripts/AssetMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+       transform.position = new Vector3(-12, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += (transform.forward * playerSpeed * Time.deltaTime);
+	}
+}
