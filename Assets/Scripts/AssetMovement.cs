f2bfbd572cc248a8fee529c49607c97191461cf6
Samuel Eng <sam.eng@icloud.com>
2015-10-24 13:33:26 -0400

    sprite sizes

--- Assets/Scripts/AssetMovement.cs
+++ Assets/Scripts/AssetMovement.cs
@@ -1,16 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class AssetMovement : MonoBehaviour {
 
     public float playerSpeed = 3.0f;
 
-	// Use this for initialization
-	void Start ()
-    {
-       transform.position = new Vector3(-12, 0, 0);
-	}
-	
 	// Update is called once per frame
 	void Update ()
     {
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	float timer = 0;
+	float timerFullValue = 1f;
+
+	float fixedXSpawnLocation = -12;
+
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	void Update () {
+		TickDownTimer();
+		if (IsTimerComplete()){
+			SpawnNewEnemy();
+			ResetTimer();
+		}
+	}
+
+
+
+	void SpawnNewEnemy(){
+		//spawn here
+		float ySpawnPosition = Random.Range(-5f, 5f);
+		Vector3 spawnPosition = new Vector3(fixedXSpawnLocation, ySpawnPosition, 0);
+		Quaternion spawnRotation = Quaternion.Euler(0f, 90f, 0f);
+		GameObject.Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+	}
+
+
+	void TickDownTimer(){
+		timer -= Time.deltaTime;
+	}
+
+
+	bool IsTimerComplete(){
+		return (timer < 0);
+	}
+
+
+	void ResetTimer(){
+		timer = timerFullValue;
+	}
+}
